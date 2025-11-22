8bfa81a2d27b722d741da4ba32c553cda38202e3
twtest <tw@test.com>
2025-11-22 13:13:20 -0500

    code update for ch8_l07

--- ch_08_decorators/l07_lru_cache.py
+++ ch_08_decorators/l07_lru_cache.py
@@ -3,10 +3,8 @@ from functools import lru_cache
 
 @lru_cache
 def is_palindrome(word):
-    if len(word) < 2:
+    if len(word) <= 1:
         return True
-    if len(word) == 2:
-        return word[0] == word[1]
     elif word[0] == word[-1]:
         return is_palindrome(word[1:-1])
     else:
